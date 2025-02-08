564498253c93d87f9e5f517ddcadcb495edf3c74
NeonKazuha <tanudeshtd2005@gmail.com>
2025-02-08 17:22:58 +0530

    Work Experience done

--- ts/app/page.tsx
+++ ts/app/page.tsx
@@ -3,10 +3,11 @@ import type { NextPage } from 'next'
 import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About"
 
 export default function Home() {
   return (
-    <div className="bg-[#1A1A1A] text-white h-screen snap-y snap-mandatory 
+    <div className="bg-[#F8F9FA] text-darkblack h-screen snap-y snap-mandatory 
     overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-darkGreen/80">
         <Head>
           <title>Tanmay Deshmukh</title>
@@ -15,11 +16,14 @@ export default function Home() {
         <Header />
 
         {/* Hero */}
-        <section id="hero">
+        <section id="hero" className="snap-start">
           <Hero />
         </section>
 
         {/* About */}
+        <section id="about" className="snap-center">
+          <About />
+        </section>
 
         {/* Experiences */}
 

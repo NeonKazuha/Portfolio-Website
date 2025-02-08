8c1dbef1e526bac2aa464b3e39847862a951ccfd
NeonKazuha <tanudeshtd2005@gmail.com>
2025-02-08 14:26:31 +0530

    Socials

--- ts/components/Header.tsx
+++ ts/components/Header.tsx
@@ -5,8 +5,33 @@ type Props = {}
 export default function Header({}: Props) {
   return (
     <header>
-        <div>
+        <div className="flex flex-row items-center">
             {/* Socials */}
+            <SocialIcon 
+                url="https://www.linkedin.com/in/tanmay-deshmukh-59a0b8275/" 
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <SocialIcon 
+                url="https://www.linkedin.com/in/tanmay-deshmukh-59a0b8275/" 
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <SocialIcon 
+                url="https://www.linkedin.com/in/tanmay-deshmukh-59a0b8275/" 
+                fgColor='gray'
+                bgColor='transparent'
+            />
+
+        <div>
+            <SocialIcon
+             
+                    url="https://www.linkedin.com/in/tanmay-deshmukh-59a0b8275/" 
+                    fgColor='gray'
+                    bgColor='transparent'
+            />
+        </div>
+
 
         </div>
     </header>

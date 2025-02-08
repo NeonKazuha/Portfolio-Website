bbfe22073a7c2e4c2be5cdd782727bf3f8c99c04
NeonKazuha <tanudeshtd2005@gmail.com>
2025-02-08 15:32:01 +0530

    Fixed Error

--- ts/components/Header.tsx
+++ ts/components/Header.tsx
@@ -1,11 +1,31 @@
+'use client'
+
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
-type Props = {}
+import { motion } from "framer-motion";
+
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-        <div className="flex flex-row items-center">
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl 
+    mx-auto z-20 xl:items-center">
+        <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+        >
             {/* Socials */}
             <SocialIcon 
                 url="https://www.linkedin.com/in/tanmay-deshmukh-59a0b8275/" 
@@ -22,7 +42,7 @@ export default function Header({}: Props) {
                 fgColor='gray'
                 bgColor='transparent'
             />
-        </div>
+        </motion.div>
 
         <div className="flex flex-row items-center text-gray-400 cursor-pointer">
             <SocialIcon

c8f810ada9000e01f841bb5522752ef2749343c2
NeonKazuha <tanudeshtd2005@gmail.com>
2025-02-08 15:38:25 +0530

    Intitalised Hero Component

--- ts/app/page.tsx
+++ ts/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import type { NextPage } from 'next'
 import Head from "next/head";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
@@ -10,13 +11,12 @@ export default function Home() {
           <title>Tanmay Deshmukh</title>
         </Head>
 
-      
-
-        {/* Header */}
         <Header />
 
         {/* Hero */}
-
+        <section id="hero">
+          <Hero />
+        </section>
 
         {/* About */}
 
